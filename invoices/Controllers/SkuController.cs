73862206331381bf2ca483b33824a237306518b8
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-12-12 23:01:45 -0500

    metodos sku

--- invoices/Controllers/SkuController.cs
+++ invoices/Controllers/SkuController.cs
@@ -2,11 +2,12 @@
 using invoices.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace invoices.Controllers
 {
     [ApiController]
-    [Route("sku")]
+    [Route("api/sku")]
     public class SkuController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -16,23 +17,30 @@ namespace invoices.Controllers
         }
 
         // GET: SkuController
-        public async Task<ActionResult> Index()
+        [HttpGet]
+        public async Task<ActionResult> Index([FromQuery] int page = 1, [FromQuery] int pageSize = 15)
         {
-            var skus =  _context.sku.GetAsyncEnumerator();
+            var skus = await _context.sku.OrderBy(x => x.Id).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
             return Ok(skus);
         }
 
         // GET: SkuController/Details/5
-        public ActionResult Details(int id)
+        [HttpGet("{id}/show")]
+        public async Task<ActionResult> Details(int id)
         {
-            return View();
+            var sku = await _context.sku.FirstOrDefaultAsync(x => x.Id == id);
+            if (sku == null)
+                return NotFound();
+            else
+                return Ok(sku);
         }
 
-        // GET: SkuController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //    // GET: SkuController/Create
+        //    [HttpGet]
+        //    public ActionResult Create()
+        //    {
+        //        return View();
+        //    }
 
         // POST: SkuController/Create
         [HttpPost]
@@ -41,55 +49,66 @@ namespace invoices.Controllers
         {
             try
             {
-               await _context.sku.AddAsync(sku);
+                _context.sku.Add(sku);
+                await _context.SaveChangesAsync();
                 return Ok();
             }
             catch
             {
-                return View();
+                return BadRequest();
             }
         }
 
         // GET: SkuController/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet("{id}/edit")]
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            var sku = await _context.sku.FirstOrDefaultAsync(x => x.Id == id);
+            if (sku == null)
+                return NotFound();
+            else
+                return Ok(sku);
         }
 
-        // POST: SkuController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //    // POST: SkuController/Edit/5
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Edit(int id, IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
 
         // GET: SkuController/Delete/5
-        public ActionResult Delete(int id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            var sku = await _context.sku.FirstOrDefaultAsync(x => x.Id == id);
+            if (sku == null)
+                return NotFound();
+            else
+                return Ok(sku);
         }
 
-        // POST: SkuController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //    // POST: SkuController/Delete/5
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public ActionResult Delete(int id, IFormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
     }
 }
